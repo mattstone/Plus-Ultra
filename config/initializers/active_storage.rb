ea826cbc900cc7c05e3ab39573f5f62c09557172
Matt Stone <matt@mybond.com>
2023-10-31 11:45:53 +1100

    More HEIC updates

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,5 @@
-Rails.application.config.active_storage.previewers << HEICPreviewer
+Rails.application.configure do
+  config.active_storage.previewers << HeicPreviewer
+  config.active_storage.variable_content_types << "image/heic"
+  config.active_storage.variable_content_types << "image/heif"
+end
