a5986ea904630a339a15b8a23662fcbb30183652
Matt Stone <matt@mybond.com>
2023-11-28 20:40:20 +1100

    Removed UTC offset from user.time_zone as this changes with daylight savings

--- db/schema.rb
+++ db/schema.rb
@@ -253,7 +253,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_064219) do
     t.string "stripe_customer_id"
     t.string "stripe_payment_method"
     t.integer "campaign_id"
-    t.string "time_zone", default: "(GMT+10:00) Australia/Sydney"
+    t.string "time_zone", default: "Australia/Sydney"
     t.index ["campaign_id"], name: "index_users_on_campaign_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["one_time_code"], name: "index_users_on_one_time_code"
